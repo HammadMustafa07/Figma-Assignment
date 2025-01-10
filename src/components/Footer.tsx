9c791c8c935536ccb26dcdfe6f737296505c94bf
HammadMustafa07 <hammadmustafa6638@gmail.com>
2025-01-10 15:29:40 +0500

    initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,17 @@
 
 
-export default function Footer () {
+export default function Footer() {
     return (
-        <div className="">
-
+        <div className="h-[243px] w-[1440px] bg-[#f4f4f5] flex flex-col justify-center items-center gap-[20px]">
+            <div className="h-[30px] w-[279px] flex justify-evenly  items-center">
+            <i className="i bi bi-facebook"></i>
+            <i className="i bi bi-instagram"></i>
+            <i className="i bi bi-twitter"></i>
+            <i className="i bi bi-linkedin"></i>
+            </div>
+            <p className="text-[22px] text-[#000000]">
+                Copyright ©2020 All rights reserved
+            </p>
         </div>
     )
 }
